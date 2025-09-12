6f4f42e4b5060d87df66e117ffa4e2b720aad6ce
unknown <emmanueladesipe01@gmail.com>
2025-09-12 06:21:19 +0100

    fixed cart serializer fields

--- E-commerce/backend/api/serializers.py
+++ E-commerce/backend/api/serializers.py
@@ -46,17 +46,20 @@ class CategorySerializer(serializers.ModelSerializer):
         fields = ["id", "name", "items"]
 
 class CartItemSerializer(serializers.ModelSerializer):
-    product_name = serializers.CharField(source="product.name", read_only=True)
-    product_price = serializers.DecimalField(source="product.price", max_digits=10, decimal_places=2, read_only=True)
+    #product_name = serializers.CharField(source="product.name", read_only=True)
+    #description = serializers.CharField(source="product.description", read_only=True)
+    #product_price = serializers.DecimalField(source="product.price", max_digits=10, decimal_places=2, read_only=True)
+    product = ProductSerializer(read_only=True)
     total_price = serializers.SerializerMethodField()
-    #product = ProductSerializer(many=False, read_only=True)
+    #image = serializers.SerializerMethodField()
     
     class Meta:
         model = CartItem
-        fields = ["id", "product", "product_name", "product_price", "quantity", "total_price"]
+        fields = ["id", "product", "quantity", "total_price"]
     
     def get_total_price(self, obj):
         return obj.quantity * obj.product.price
+    
 
 class RestaurantSerializer(serializers.ModelSerializer):
     is_favorite = serializers.SerializerMethodField()
