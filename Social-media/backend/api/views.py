a99e0573d007d662486bc4a56c79aa00f0c9bea8
unknown <emmanueladesipe01@gmail.com>
2025-09-17 23:15:29 +0100

    added index page for api

--- Social-media/backend/api/views.py
+++ Social-media/backend/api/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+#
+from .models import Restaurant, Cart, Product, CartItem
+from .auth import User
 
-# Create your views here.
+#rest_framework
+from rest_framework import viewsets, permissions, status, generics
+from rest_framework.decorators import api_view, action
+from rest_framework.response import Response
+from .serializers import RestaurantSerializer, RestaurantListSerializer, RestaurantProductSerializer, CartSerializer, CartItemSerializer
+
+#rest_framework simple jwts
+
+
+#View Functions
+@api_view(["GET"])
+def index(request):
+    context = {
+        'list of url routes': '/index',
+    }
+    return Response(context)
