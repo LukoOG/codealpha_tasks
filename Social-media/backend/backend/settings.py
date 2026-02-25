a56acef8b1686ba405a25fb0992fa70180e48f99
LukoOG <emmanueladesipe01@gmail.com>
2026-02-25 17:30:59 +0100

    Fixed default file storage setting

--- Social-media/backend/backend/settings.py
+++ Social-media/backend/backend/settings.py
@@ -246,10 +246,13 @@ JAZZMIN_UI_TWEAKS = {
     },
 }
 
-CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
-    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
-    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+STORAGES = {
+    "default": {
+        "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+    },
 }
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
